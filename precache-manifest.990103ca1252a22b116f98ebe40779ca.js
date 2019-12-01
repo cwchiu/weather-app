27cbf74107ee40514bff42c46f3340fea4e60fcd
arick <arick@grr.la>
2019-12-01 08:24:53 +0800

    Updates

---
+++ precache-manifest.990103ca1252a22b116f98ebe40779ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3c7611770a43d4250da399e5615e4d",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "f4b3ceacf2c5bf2acb63",
+    "url": "/weather-app/static/css/main.871e9c7b.chunk.css"
+  },
+  {
+    "revision": "b6f5c0d9e9ff1baaec9f",
+    "url": "/weather-app/static/js/2.e2b896d9.chunk.js"
+  },
+  {
+    "revision": "f4b3ceacf2c5bf2acb63",
+    "url": "/weather-app/static/js/main.61e8ed2e.chunk.js"
+  },
+  {
+    "revision": "493d27131f8ef863d325",
+    "url": "/weather-app/static/js/runtime-main.392162b8.js"
+  },
+  {
+    "revision": "5c66b1345ba4954b591a9abd473fec8e",
+    "url": "/weather-app/static/media/cloudy.5c66b134.svg"
+  },
+  {
+    "revision": "881734c06e6bbfd0addb6e1459f25d60",
+    "url": "/weather-app/static/media/rain.881734c0.svg"
+  }
+]);
